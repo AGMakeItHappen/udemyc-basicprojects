bcf9aeaba629b1070adee0266a54b05e947133c1
Aryan Goswami <aryangoswami@Aryans-MacBook-Pro.local>
2025-06-03 21:56:14 +0530

    learning

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() 
+{
+    
+    int name;
+
+    std::cout << "Your name is ";
+    std::cin >> name;
+
+    std::cout<<name;
+
+    return 0;
+}
